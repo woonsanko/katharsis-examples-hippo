02610b5007aca4256e14d5a953f3f8685a221476
Woonsan Ko <w.ko@onehippo.com>
2015-09-28 18:43:38 -0400

    initial project creation

---
+++ site/src/main/java/com/github/woonsanko/katharsis/examples/hippo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.github.woonsanko.katharsis.examples.hippo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="katharsisexampleshippo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
